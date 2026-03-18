283aa5fc2fab9051d56e93d0d37bc3cec42d84cd
Amrita <167407965+Rajamrita@users.noreply.github.com>
2026-03-18 23:49:25 +0530

    Delete ArrayExample.java

--- ArrayExample.java
+++
@@ -1,9 +0,0 @@
-public class ArrayExample {
-    public static void main(String[] args) {
-        int[] arr = {10, 20, 30, 40};
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
-    }
-}
