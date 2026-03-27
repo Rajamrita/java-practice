1f8a48d1df03da18efb84c79d7746dee7dcdc36f
Amrita <167407965+Rajamrita@users.noreply.github.com>
2026-03-27 09:06:19 +0530

    Add files via upload

---
+++ java-beginners/DeleteNode.java
@@ -0,0 +1,26 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class DeleteNode {
+    public static void main(String[] args) {
+
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+
+        // delete first node
+        head = head.next;
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+    }
+}
