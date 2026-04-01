9fb38862eef617270a540f054032d6bc6de23c93
Amrita <167407965+Rajamrita@users.noreply.github.com>
2026-04-01 07:18:45 +0530

    Add files via upload

---
+++ java-beginners/ReverseStringRecursion.java
@@ -0,0 +1,14 @@
+public class ReverseStringRecursion {
+
+    static void reverse(String str, int i) {
+        if (i < 0) return;
+
+        System.out.print(str.charAt(i));
+        reverse(str, i - 1);
+    }
+
+    public static void main(String[] args) {
+        String str = "hello";
+        reverse(str, str.length() - 1);
+    }
+}
