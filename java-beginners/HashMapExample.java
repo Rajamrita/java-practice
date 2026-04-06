6c2636c6dd05aec7b2ee72a0aa2115850064134f
Amrita <167407965+Rajamrita@users.noreply.github.com>
2026-04-06 14:16:59 +0530

    Add files via upload

---
+++ java-beginners/HashMapExample.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> map = new HashMap<>();
+
+        map.put(1, "A");
+        map.put(2, "B");
+
+        System.out.println(map);
+    }
+}
