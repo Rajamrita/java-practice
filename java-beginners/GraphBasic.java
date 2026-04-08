b1519316411d48de112b57391e2b1de167223d30
Amrita <167407965+Rajamrita@users.noreply.github.com>
2026-04-08 11:00:59 +0530

    Add files via upload

---
+++ java-beginners/GraphBasic.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class GraphBasic {
+    public static void main(String[] args) {
+
+        int V = 4;
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < V; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        graph.get(0).add(1);
+        graph.get(1).add(2);
+        graph.get(2).add(3);
+
+        System.out.println(graph);
+    }
+}
