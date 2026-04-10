aed648c228839feb6fa289f52588004a1f5d12b6
Amrita <167407965+Rajamrita@users.noreply.github.com>
2026-04-10 12:07:05 +0530

    Add files via upload

---
+++ java-beginners/BitManipulation.java
@@ -0,0 +1,10 @@
+public class BitManipulation {
+    public static void main(String[] args) {
+        int n = 5;
+
+        if ((n & 1) == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+}
