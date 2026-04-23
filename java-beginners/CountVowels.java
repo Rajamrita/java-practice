75aea31f2fe72e378132e6bbf919ecbf99787063
Amrita <167407965+Rajamrita@users.noreply.github.com>
2026-04-23 17:51:34 +0530

    Add files via upload

---
+++ java-beginners/CountVowels.java
@@ -0,0 +1,15 @@
+public class CountVowels {
+    public static void main(String[] args) {
+
+        String str = "hello world";
+        int count = 0;
+
+        for (char c : str.toCharArray()) {
+            if ("aeiouAEIOU".indexOf(c) != -1) {
+                count++;
+            }
+        }
+
+        System.out.println("Vowels: " + count);
+    }
+}
