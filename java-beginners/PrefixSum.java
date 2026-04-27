b9bae8bde694056aad70a2ba88cfbdf1a6a5680b
Amrita <167407965+Rajamrita@users.noreply.github.com>
2026-04-27 12:41:23 +0530

    Add files via upload

---
+++ java-beginners/PrefixSum.java
@@ -0,0 +1,15 @@
+public class PrefixSum {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4};
+
+        int[] prefix = new int[arr.length];
+        prefix[0] = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+
+        System.out.println(prefix[3]); // sum of 0 to 3
+    }
+}
