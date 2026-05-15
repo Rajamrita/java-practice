301a086f6bf12174763080971708d935ec45e769
Amrita <167407965+Rajamrita@users.noreply.github.com>
2026-05-15 09:28:17 +0530

    Add files via upload

---
+++ java-beginners/SimpleStack.java
@@ -0,0 +1,9 @@
+import java.util.Stack;
+public class SimpleStack {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(10);
+        stack.push(20);
+        System.out.println(stack.pop());
+    }
+}
