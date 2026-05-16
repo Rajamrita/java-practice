25b148d78fb9c39f8c62965a753d976c52b2b017
Amrita <167407965+Rajamrita@users.noreply.github.com>
2026-05-16 10:14:18 +0530

    Add files via upload

---
+++ java-beginners/PositiveNegative.java
@@ -0,0 +1,11 @@
+public class PositiveNegative {
+    public static void main(String[] args) {
+        int n = -5;
+        if (n > 0)
+            System.out.println("positive");
+        else if (n < 0)
+            System.out.println("Negative");
+        else 
+            System.out.println("zero");
+    }
+}
