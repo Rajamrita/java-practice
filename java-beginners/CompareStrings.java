12c1154e70d5c74b01432cd8b49fec8ee7fbacec
Amrita <167407965+Rajamrita@users.noreply.github.com>
2026-05-18 17:16:30 +0530

    Add files via upload

---
+++ java-beginners/CompareStrings.java
@@ -0,0 +1,12 @@
+public class CompareStrings {
+    public static void main(String[] args) {
+
+        String a = "java";
+        String b = "java";
+
+        if (a.equals(b))
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+    }
+}
