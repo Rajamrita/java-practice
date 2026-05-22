43be65a65ad67af1d3c13ff6b4752877e650d67f
Amrita <167407965+Rajamrita@users.noreply.github.com>
2026-05-22 16:57:48 +0530

    Add files via upload

---
+++ java-beginners/SquareFunction.java
@@ -0,0 +1,9 @@
+public class SquareFunction {
+    static int square(int n) {
+        return n * n;
+    }
+    public static void main(String[] args) {
+        System.out.println(square(4));
+    }
+    
+}
