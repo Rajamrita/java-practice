ebf43db929694515046ef9b3f63af637f4d13a00
Amrita <167407965+Rajamrita@users.noreply.github.com>
2026-05-24 18:12:13 +0530

    Add files via upload

---
+++ java-beginners/ReplaceWord.java
@@ -0,0 +1,7 @@
+public class ReplaceWord {
+    public static void main(String[] args) {
+        String str = "I like Java";
+        str = str.replace("Java", "Python");
+        System.out.println(str);
+    }  
+}
