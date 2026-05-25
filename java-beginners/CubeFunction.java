81617d16572e5f3e5aa3c8059806a8cb8ec0cc8d
Amrita <167407965+Rajamrita@users.noreply.github.com>
2026-05-25 17:59:16 +0530

    Add files via upload

---
+++ java-beginners/CubeFunction.java
@@ -0,0 +1,11 @@
+public class CubeFunction {
+
+    static int cube(int n) {
+        return n * n * n;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(cube(3));
+    }
+}
