ec991131069b642db9e219fd21fd2c39e06c227b
Amrita <167407965+Rajamrita@users.noreply.github.com>
2026-05-26 20:47:51 +0530

    Add files via upload

---
+++ SimpleSelectionSort.java
@@ -0,0 +1,18 @@
+public class SimpleSelectionSort {
+     public static void main(String[] args) {
+        int[] arr = {29, 18, 14, 37};
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j]  < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+        for (int num : arr)
+            System.out.print(num + " ");
+     }
+}
