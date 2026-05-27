ca4e5aa717ee749eef7ea2776d0ca8009ca8c4e0
Amrita <167407965+Rajamrita@users.noreply.github.com>
2026-05-27 18:44:57 +0530

    Add files via upload

---
+++ java-beginners/LargestElement.java
@@ -0,0 +1,12 @@
+public class LargestElement {
+    public static void main(String[] args) {
+        int[]arr = {10, 45, 7, 90, 23};
+        int largest = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+        System.out.println("Largest element: " + largest);
+    }  
+}
