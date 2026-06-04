96698e157a2a831bf6705a0cba08859084b43c28
Amrita <167407965+Rajamrita@users.noreply.github.com>
2026-06-04 23:54:28 +0530

    Add files via upload

---
+++ java-beginners/FirstUniqueCharacter.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class FirstUniqueCharacter {
+
+    public static void main(String[] args) {
+
+        String str = "swiss";
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char ch : str.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for (char ch : str.toCharArray()) {
+            if (map.get(ch) == 1) {
+                System.out.println("First Unique Character: " + ch);
+                break;
+            }
+        }
+    }
+}
