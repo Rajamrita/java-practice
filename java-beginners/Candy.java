634ec9aaa9b9cbda74e2021a222a03180be0fc9a
Amrita <167407965+Rajamrita@users.noreply.github.com>
2026-07-14 20:26:25 +0530

    Add files via upload

---
+++ java-beginners/Candy.java
@@ -0,0 +1,32 @@
+public class Candy {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candies = new int[n];
+        // Give 1 candy to eyeryone
+        for (int i = 0; i < n; i++) {
+            candies[i] = 1;
+        }
+        // Left to Right
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+        // Right to Left
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+        int total = 0;
+        for(int c : candies) {
+            total += c;
+        }
+        return total;
+    }
+    public static void main(String[] args) {
+        Candy obj = new Candy();
+        int[] ratings = {1, 0, 2};
+        System.out.println(obj.candy(ratings));
+    }
+}
