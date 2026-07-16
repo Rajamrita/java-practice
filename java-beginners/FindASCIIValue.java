23968049f759202740cef16b9673583396b9e028
Amrita <167407965+Rajamrita@users.noreply.github.com>
2026-07-17 02:42:30 +0530

    Add files via upload

---
+++ java-beginners/FindASCIIValue.java
@@ -0,0 +1,9 @@
+public class FindASCIIValue {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int ascii = ch;
+        System.out.println("ASCII value of " + ch + " is: " + ascii);
+        
+    }
+    
+}
